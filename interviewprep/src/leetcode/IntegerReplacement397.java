ea4bce96741879c68ab0549a52e746029c907ec7
sriiskanth <sri.is.kanth@gmail.com>
2017-01-31 22:54:06 +0530

    Commit from leetcode

---
+++ interviewprep/src/leetcode/IntegerReplacement397.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * Created by srikanth on 1/29/2017.
+ */
+public class IntegerReplacement397 {
+    public static void main(String args[]) {
+
+    }
+}
--- interviewprep/src/leetcode/Sample.java
+++ interviewprep/src/leetcode/Sample.java
@@ -3,7 +3,7 @@ package leetcode;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World::	" + 1234 / 2);
 		
 	}
 
