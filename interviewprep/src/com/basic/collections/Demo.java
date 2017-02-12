08fe4a85cec050fdcabaa860ecd8cdfe7ffd4f1a
sriiskanth <sri.is.kanth@gmail.com>
2017-02-12 15:28:27 +0530

    Collections

---
+++ interviewprep/src/com/basic/collections/Demo.java
@@ -0,0 +1,10 @@
+package com.basic.collections;
+
+/**
+ * Created by srikanth on 2/12/2017.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.print("Demo");
+    }
+}
